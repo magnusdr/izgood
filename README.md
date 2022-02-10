47d127c101af10d1e80f0420c70d6d48b107daa1
Magnus <>
2022-02-10 15:41:32 +0100

    Add support for using js objects in addition to FormData, and showing all error messages in a list

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # `izgood`
+
 ![Dog approves](media/dog.webp)
 
 ## Check if data `izgood` in three simple steps! ✨
+
 1. Use React hook `const [validate, NotGood] = useNotGood(rules)`
 1. Place the `<NotGood />` components where you want the error messages to appear
 1. Use the `validate(formdata)` callback before submitting your form to the API. It returns `false` if data iz not good.
@@ -9,57 +11,65 @@
 ## Example usage
 
 ```jsx
-import { useNotGood, izNotEmpty } from 'izgood'
+import { useNotGood, izNotEmpty } from "izgood";
 
 export default function GiftYourFriend() {
-    const [validate, NotGood] = useNotGood([
-        ["email", izNotEmpty],
-        ["gift_code", izNotEmpty, "Empty gift iz kinda evil? 🙄"]
-    ])
+  const [validate, NotGood] = useNotGood([
+    ["email", izNotEmpty],
+    ["gift_code", izNotEmpty, "Empty gift iz kinda evil? 🙄"],
+  ]);
 
-    const handleSubmit = useCallback((e) => {
-        e.preventDefault()
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
 
-        const formdata = FormData(e.target)
-        if(validate(formdata)) {
-            return
-        }
+      const formdata = FormData(e.target);
+      if (validate(formdata)) {
+        return;
+      }
 
-        // ...API request hidden for brevity
-    }, [ validate ])
+      // ...API request hidden for brevity
+    },
+    [validate]
+  );
 
-    return <div>
-        <h1>Send a gift to your friend!</h1>
-        <form onSubmit={handleSubmit}>
-            <input name="email" />
-            <NotGood name="email" />
-            <input name="gift_code" />
-            <NotGood name="gift_code" />
-        </form>
+  return (
+    <div>
+      <h1>Send a gift to your friend!</h1>
+      <form onSubmit={handleSubmit}>
+        <input name="email" />
+        <NotGood name="email" />
+        <input name="gift_code" />
+        <NotGood name="gift_code" />
+      </form>
     </div>
+  );
 }
-
 ```
 
 ## API Reference
+
 ```js
-const [validate, NotGood] = useNotGood(rules)
+const [validate, NotGood] = useNotGood(rules);
 ```
 
 ### `rules` format
+
 Rules is a list of objects
+
 ```ts
 type ValidationRule = {
-    name: string, // The name of the <input /> field to validate
-    whats_wrong: (value: any, args: any) => string | undefined,
-    message?: string, // Optional custom error message
-    args?: any, // Optional args object passed to whats_wrong
-}
+  name: string; // The name of the <input /> field to validate
+  whats_wrong: (value: any, args: any) => string | undefined;
+  message?: string; // Optional custom error message
+  args?: any; // Optional args object passed to whats_wrong
+};
 ```
 
 `whats_wrong` is the actual implementation of the validation. `izgood` comes with some built-in validation functions like `izNotEmpty`, but you can extend this easily by creating your own functions with the same signature `(value: any, args: any) => string | undefined`. Returning `undefined` means validation passed, `string` is the error message when validation fails.
 
 Rules can also be a list of tuples (arrays) for shorter syntax
+
 ```ts
 [
     string, // name
@@ -72,19 +82,30 @@ Rules can also be a list of tuples (arrays) for shorter syntax
 ### The `validate` callback
 
 `validate` has the following function signature:
+
 ```ts
-function validate(formData: FormData): boolean
+function validate(formData: FormData): boolean;
 ```
 
-It returns `fales` if validation failed, and `true` if all rules passed. 
+It returns `fales` if validation failed, and `true` if all rules passed.
+
+It is supported to use regular javascript objects instead of Formdata, but you have to do some dirty type casting.
+
+```ts
+validate(data as any);
+```
 
 Calling this function will trigger a re-rendering of your component, updating all the `<NotGood />` components.
 
 ### `<NotGood />` component
-This component takes one property `name: string`: 
+
+This component takes one property `name?: string`.
+
 ```jsx
 <NotGood name="email" />
 ```
+
+If you omit `name`, no filtering will be done, and all error messages will be displayed inside the component.
 
 You can also pass any other property to the underlying `<div>` like `className`, `styles` etc.
 
@@ -94,6 +115,6 @@ Whether you choose to give this component an extra `className`s, it always has `
 /* global.css */
 
 .notgood {
-    color: red;
+  color: red;
 }
 ```
--- izgood.tsx
+++ izgood.tsx
@@ -1,120 +1,151 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useState } from "react";
 
 type ValidationRule = {
-    name: string,
-    whats_wrong: (value: any, args: any) => string | undefined,
-    message?: string,
-    args?: any,
-}
+  name: string;
+  whats_wrong: (value: any, args: any) => string | undefined;
+  message?: string;
+  args?: any;
+};
 
-type ValidationRuleTuple = [string, (value: any, args: any) => string | undefined, string?, any?]
+type ValidationRuleTuple = [
+  string,
+  (value: any, args: any) => string | undefined,
+  string?,
+  any?
+];
 
-type Rules = (ValidationRule | ValidationRuleTuple)[]
+type Rules = (ValidationRule | ValidationRuleTuple)[];
 
 type FormError = {
-    name: string,
-    message: string
-}
+  name: string;
+  message: string;
+};
 
 function izgood(formdata: FormData, rules: Rules): FormError[] | undefined {
-    let errors: FormError[] = []
+  let errors: FormError[] = [];
 
-    for (let i = 0; i < rules.length; i++) {
-        let rule = rules[i];
-        if(Array.isArray(rule)) {
-            rule = {
-                name: rule[0],
-                whats_wrong: rule[1],
-                message: rule[2],
-                args: rule[3],
-            }
-        }
-        
-        let err_msg = rule.whats_wrong(formdata.get(rule.name), rule.args)
-        if(err_msg) {
-            errors.push({
-                name: rule.name,
-                message: rule.message ?? err_msg
-            })
-        }
+  for (let i = 0; i < rules.length; i++) {
+    let rule = rules[i];
+    if (Array.isArray(rule)) {
+      rule = {
+        name: rule[0],
+        whats_wrong: rule[1],
+        message: rule[2],
+        args: rule[3],
+      };
     }
 
-    return errors.length === 0 ? undefined : errors
+    let err_msg = rule.whats_wrong(
+      formdata instanceof FormData
+        ? formdata.get(rule.name)
+        : formdata[rule.name],
+      rule.args
+    );
+    if (err_msg) {
+      errors.push({
+        name: rule.name,
+        message: rule.message ?? err_msg,
+      });
+    }
+  }
+
+  return errors.length === 0 ? undefined : errors;
 }
 
 type NotGoodProps = {
-    name: string,
-    errors?: FormError[],
-    className?: string,
-    [key: string]: any,
-}
+  name?: string;
+  errors?: FormError[];
+  className?: string;
+  [key: string]: any;
+};
 
-function NotGood({ name, errors, className, ...restProps } : NotGoodProps) {
-    if(errors) {
-        let my_err = errors.filter((e) => e.name === name)?.[0]
-        if(my_err) {
-            return <div className={`notgood ${className}`} {...restProps}>
-                {my_err.message}
-            </div>
-        }
+function NotGood({ name, errors, className, ...restProps }: NotGoodProps) {
+  if (name === undefined) {
+    return (
+      <div className={`notgood ${className}`} {...restProps}>
+        <ul>
+          {errors?.map((e) => (
+            <li>{e.message}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else if (errors) {
+    let my_err = errors.filter((e) => e.name === name)?.[0];
+    if (my_err) {
+      return (
+        <div className={`notgood ${className}`} {...restProps}>
+          {my_err.message}
+        </div>
+      );
     }
-    return <></>
+  }
+  return <></>;
 }
 
-export function useNotGood(rules: Rules): [(d: FormData) => boolean, typeof NotGood] {
-    const [errors, set_errors] = useState<FormError[] | undefined>()
+export default function useNotGood(
+  rules: Rules
+): [(d: FormData) => boolean, typeof NotGood] {
+  const [errors, set_errors] = useState<FormError[] | undefined>();
 
-    const validate = useCallback((formdata: FormData) => {
-        let result = izgood(formdata, rules)
-        if(result === undefined) {
-            return true
-        }else {
-            set_errors(result)
-            return false
-        }
-    }, [ rules ])
+  const validate = useCallback(
+    (formdata: FormData) => {
+      let result = izgood(formdata, rules);
+      if (result === undefined) {
+        return true;
+      } else {
+        set_errors(result);
+        return false;
+      }
+    },
+    [rules]
+  );
 
-    return [validate, function({ name, ...restProps } : NotGoodProps) {
-        return <NotGood 
-            name={name} 
-            errors={errors}
-            {...restProps} />
-    }]
+  return [
+    validate,
+    function ({ name, ...restProps }: NotGoodProps) {
+      return <NotGood name={name} errors={errors} {...restProps} />;
+    },
+  ];
 }
 
 /* 
  Built in validation functions
 */
 export function izNotEmpty(value: any, args: any): string | undefined {
-    if(typeof value === "object" && value?.size === 0) {
-        return "This field cannot be empty"
-    }
+  if (typeof value === "object" && value?.size === 0) {
+    return "This field cannot be empty";
+  }
 
-    const stripped_value = value && typeof value === "string" ? value.trim() : value
+  const stripped_value =
+    value && typeof value === "string" ? value.trim() : value;
 
-    return value == null || stripped_value.length === 0 ? "This field cannot be empty" : undefined
+  return value == null || stripped_value.length === 0
+    ? "This field cannot be empty"
+    : undefined;
 }
 
 export function izEmail(value: any, args: any): string | undefined {
-    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (typeof value === "string" && value.match(regex)) {
-        return undefined
-    } else {
-        return "Invalid email address"
-    }
+  if (typeof value === "string" && value.match(regex)) {
+    return undefined;
+  } else {
+    return "Invalid email address";
+  }
 }
 
 export function izMoreThan(value: any, args: any): string | undefined {
-    const valueAsNumber = parseInt(value)
+  const valueAsNumber = parseInt(value);
 
-    if (valueAsNumber !== NaN) {
-        if(args.min != null && valueAsNumber < args.min) {
-            return `This has to be minimum ${args.min}`
-        }else {
-            return undefined
-        }
+  if (valueAsNumber !== NaN) {
+    if (args.min != null && valueAsNumber < args.min) {
+      return `This has to be minimum ${args.min}`;
     } else {
-        return `This has to be minimum ${args.min}`
+      return undefined;
     }
+  } else {
+    return `This has to be minimum ${args.min}`;
+  }
 }
